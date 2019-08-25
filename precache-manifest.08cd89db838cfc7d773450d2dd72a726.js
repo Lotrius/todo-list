adbe7ce2bb83aeea1ff4dd54743cd9820f1d198d
Somesk278 <skim7420@gmail.com>
2019-08-24 20:17:26 -0400

    Updates

---
+++ precache-manifest.08cd89db838cfc7d773450d2dd72a726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7772e1c014b215eb752c0b9bb4eef4b3",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "d6d0cc7f76fb706dfb69",
+    "url": "/todo-list/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "d7e3909a944c56070936",
+    "url": "/todo-list/static/css/main.6ff64552.chunk.css"
+  },
+  {
+    "revision": "d6d0cc7f76fb706dfb69",
+    "url": "/todo-list/static/js/2.34af9f53.chunk.js"
+  },
+  {
+    "revision": "d7e3909a944c56070936",
+    "url": "/todo-list/static/js/main.d6ee490c.chunk.js"
+  },
+  {
+    "revision": "fc9aed9862fca12f280d",
+    "url": "/todo-list/static/js/runtime~main.07323a70.js"
+  }
+]);
